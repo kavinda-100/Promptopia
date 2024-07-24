cf22fc9419b74d6c4515a4b956793cd43cde14f9
kavinda-100 <kavindarathnayake100@gmail.com>
2024-07-24 20:03:38 +0530

    chore: Update Heder component with mobile authentication and profile page

--- components/Heder.tsx
+++ components/Heder.tsx
@@ -6,11 +6,29 @@ import Image from "next/image";
 import { useState, useEffect, Dispatch, SetStateAction } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 import { Button } from "./ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { AlignRight } from "lucide-react";
 
 const Heder = () => {
-  const [open, setOpen] = useState(false);
+  const isUserLoggedIn = true;
+  // get session
+  const [providers, setProviders] = useState<any>(null);
 
+  // get providers
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const providers = await getProviders();
+      setProviders(providers);
+    };
+
+    fetchProviders();
+  }, []);
   return (
     <header className="flex w-full mb-16 justify-between items-center h-[70px] shadow-sm px-5">
       {/* logo */}
@@ -22,7 +40,9 @@ const Heder = () => {
           height={30}
           className="cursor-pointer object-contain"
         />
-        <h1 className="text-2xl font-bold cursor-pointer">Promtopia</h1>
+        <h1 className="text-2xl font-bold cursor-pointer text-foreground">
+          Promtopia
+        </h1>
       </Link>
       {/* mode toggle and auth */}
       <div className="lg:flex gap-2 justify-center items-center hidden">
@@ -30,14 +50,43 @@ const Heder = () => {
         <ModeToggle />
         {/* auth */}
         <div className=" flex gap-2">
-          <Button onClick={() => signIn("google")}>Sign in</Button>
-          <Button onClick={() => signOut()} variant={"outline"}>
-            Sign out
-          </Button>
+          {/* if user log in */}
+          {isUserLoggedIn ? (
+            <>
+              <Link href={"create-post"}>
+                <Button>Create Post</Button>
+              </Link>
+              <Button onClick={() => signOut()} variant={"outline"}>
+                Sign out
+              </Button>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </>
+          ) : (
+            <>
+              {/* if not user log in */}
+              {providers &&
+                Object.values(providers).map((provider: any) => (
+                  <Button
+                    onClick={() => signIn(provider.id)}
+                    key={provider.name}
+                  >
+                    Sign in with {provider.name}
+                  </Button>
+                ))}
+              <Button onClick={() => signIn("google")}>Sign in</Button>
+            </>
+          )}
         </div>
       </div>
       <div className="lg:hidden">
-        <MobileAuth />
+        <MobileAuth
+          isUserLoggedIn={isUserLoggedIn}
+          avatarUrl="https://github.com/shadcn.png"
+          providers={providers}
+        />
       </div>
     </header>
   );
@@ -45,27 +94,67 @@ const Heder = () => {
 
 export default Heder;
 
-// for mobile view
-// {
-//   open,
-//   setOpen,
-// }: {
-//   open: boolean;
-//   setOpen: Dispatch<SetStateAction<boolean>>;
-// }
-const MobileAuth = () => {
+const MobileAuth = ({
+  isUserLoggedIn,
+  avatarUrl,
+  providers,
+}: {
+  isUserLoggedIn: boolean;
+  avatarUrl: string;
+  providers?: any;
+}) => {
   return (
     <Sheet>
-      <SheetTrigger>Open</SheetTrigger>
+      <SheetTrigger>
+        <AlignRight />
+      </SheetTrigger>
       <SheetContent>
-        {/* mode toggle */}
-        <ModeToggle />
+        <SheetHeader className="mt-6">
+          {isUserLoggedIn ? (
+            <div className="flex justify-between items-center">
+              {/* mode toggle */}
+              <ModeToggle />
+              {/* avatar */}
+              <Avatar>
+                <AvatarImage src={avatarUrl} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+          ) : (
+            <>
+              {/* mode toggle */}
+              <ModeToggle />
+            </>
+          )}
+        </SheetHeader>
         {/* auth */}
         <div className=" flex flex-col gap-3 mt-5">
-          <Button onClick={() => signIn("google")}>Sign in</Button>
-          <Button onClick={() => signOut()} variant={"outline"}>
-            Sign out
-          </Button>
+          {/* if user log in */}
+          {isUserLoggedIn ? (
+            <>
+              <Link href={"create-post"}>
+                <Button className="w-full">Create Post</Button>
+              </Link>
+              <Button onClick={() => signOut()} variant={"outline"}>
+                Sign out
+              </Button>
+            </>
+          ) : (
+            <>
+              {/* if not user log in */}
+              {providers &&
+                Object.values(providers).map((provider: any) => (
+                  <Button
+                    onClick={() => signIn(provider.id)}
+                    key={provider.name}
+                    className="w-full"
+                  >
+                    Sign in with {provider.name}
+                  </Button>
+                ))}
+              <Button onClick={() => signIn("google")}>Sign in</Button>
+            </>
+          )}
         </div>
       </SheetContent>
     </Sheet>
